cdf6bd272f186ed24a98807ffda1341931f6fb76
athdromeda <anixatho01@gmail.com>
2024-10-20 13:00:20 +0700

    feat: show notification after stacking success

--- electron/utils/stackImage.ts
+++ electron/utils/stackImage.ts
@@ -1,19 +1,34 @@
 import { spawn } from "child_process";
+import { Notification } from "electron";
 
 export function stackImage(filepath: string) {
   const newName = filepath.replace(".png", "-stacked.png");
 
   if (process.env.NODE_ENV === "production") {
-    console.log("Start stacking");
+    new Notification({
+      title: "Image Stacked",
+      body: `${filepath} stacked successfully`,
+    }).show();
+
     const cp = spawn("tts", [filepath]);
     cp.on("close", () => {
-      console.log("Stacking finished");
+      new Notification({
+        title: "Image Stacked",
+        body: `${filepath} stacked successfully`,
+      }).show();
     });
   } else {
-    console.log("start copying");
+    new Notification({
+      title: "Image Stacked",
+      body: `${filepath} stacked successfully`,
+    }).show();
+
     const cp = spawn("cp", [filepath, newName]);
     cp.on("close", () => {
-      console.log("finished");
+      new Notification({
+        title: "Image Stacked",
+        body: `${filepath} stacked successfully`,
+      }).show();
     });
   }
 }
